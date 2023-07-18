d2142636761dbe3c451ecb62d86f1fd85c07a7f7
William Gerard <whgerard1@gmail.com>
2023-07-18 13:50:18 -0400

    Add scalar multiply which saves output to separate array

--- src/L1/scal.cuh
+++ src/L1/scal.cuh
@@ -13,3 +13,15 @@ void scal(uint32_t n,
         x[ind] = alpha * x[ind];
     }
 }
+template <typename T>
+__device__
+void scal(std::uint32_t n, 
+          T alpha, 
+          T *x,
+          T *y,
+          cgrps::thread_group g = cgrps::this_thread_block())
+{
+    for(std::uint32_t ind = g.thread_rank(); ind < n; ind += g.size()){
+        y[ind] = alpha * x[ind];
+    }
+}
