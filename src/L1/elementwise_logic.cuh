749ebdadd9974a57ad18b41ed7373bc21fa2455e
William Gerard <whgerard1@gmail.com>
2023-07-18 14:59:35 -0400

    Add elementwise logic operators

---
+++ src/L1/elementwise_logic.cuh
@@ -0,0 +1,39 @@
+#include <cstdint>
+#include <cooperative_groups.h>
+namespace cgrps = cooperative_groups;
+
+template <typename T>
+__device__
+void elementwise_less_than(uint32_t N, T* a, T* b, T* c,
+                cgrps::thread_group g = cgrps::this_thread_block()) {
+    for (int i = g.thread_rank(); i < N; i += g.size()) {
+        c[i] = a[i] < b[i];
+    }
+}
+
+template <typename T>
+__device__
+void elementwise_and(uint32_t N, T* a, T* b, T* c,
+                cgrps::thread_group g = cgrps::this_thread_block()) {
+    for (int i = g.thread_rank(); i < N; i += g.size()) {
+        c[i] = a[i] && b[i];
+    }
+}
+
+template <typename T>
+__device__
+void elementwise_not(uint32_t N, T* a, T* c,
+                cgrps::thread_group g = cgrps::this_thread_block()) {
+    for (int i = g.thread_rank(); i < N; i += g.size()) {
+        c[i] = !a[i];
+    }
+}
+
+template <typename T>
+__device__
+void elementwise_mult(uint32_t N, T* a, T* b, T* c,
+                cgrps::thread_group g = cgrps::this_thread_block()) {
+    for (int i = g.thread_rank(); i < N; i += g.size()) {
+        c[i] = a[i] * b[i];
+    }
+}
