b1395237d2701c50c26f2592607a7416642e6e3c
EmreAdabag <emreadabag@gmail.com>
2023-10-29 17:37:14 -0400

    New axpby

--- src/L1/axpy.cuh
+++ src/L1/axpy.cuh
@@ -38,3 +38,23 @@ void axpy(std::uint32_t n,
         z[ind] = alpha * x[ind] + y[ind];
     }
 }
+
+/*
+    Compute the scaled sum of two vectors
+    alpha * x + beta * y
+    store the result in z
+*/
+template <typename T>
+__device__
+void axpby(std::uint32_t n, 
+          T alpha, 
+          T *x,
+		  T beta, 
+          T *y, 
+          T *z, 
+          cgrps::thread_group g = cgrps::this_thread_block())
+{
+    for(std::uint32_t ind = g.thread_rank(); ind < n; ind += g.size()){
+        z[ind] = alpha * x[ind] + beta * y[ind];
+    }
+}
