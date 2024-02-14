b5b9ae51cb8c8b920f6f0ebfb18c0b2f50fff9ba
seyoungree <seyoungree@gmail.com>
2024-02-13 20:53:11 -0500

    fixed chol time test to launch kernel only once

--- GTests/chol_time.cu
+++ GTests/chol_time.cu
@@ -6,75 +6,67 @@
 
 template <typename T> 
 __global__ 
-void global_cholDecomp_InPlace (uint32_t n,
-                        T *s_A)
+void global_cholDecomp_InPlace (int n,
+                        T *s_A, int reps)
 {
-    glass::chol_InPlace<T>(n, s_A);
+	for (int i = 0; i < reps; i++) {
+		glass::chol_InPlace<T>(n, s_A);
+		__syncthreads();
+	}
 }
 
 template <typename T> 
 __global__ 
 void global_cholDecomp_InPlace_vec (uint32_t n,
-                        T *s_A)
+                        T *s_A, int reps)
 {
-    glass::chol_InPlace_vec<T>(n, s_A);
+    for (int i = 0; i < reps; i++) {
+		glass::chol_InPlace_vec<T>(n, s_A);
+		__syncthreads();
+	}
 }
 
 int main(int argc, char *argv[]) {
-	double time_taken, total_time, avg_time, total_time_vec, avg_time_vec;
-	long len = atol(argv[1]);
-	long reps = atol(argv[2]);
+	double total_time, avg_time, total_time_vec, avg_time_vec;
 	struct timespec start, end;
 	double *d_d;
 	long i;
 
+	long len = atol(argv[1]);
+	long reps = atol(argv[2]);
 	double *h_d =(double*)malloc(sizeof(double)*pow(len,2));
     double *res = (double*)malloc(sizeof(double)*pow(len,2));
-	cudaMalloc(&d_d, pow(len,2) * sizeof(double));
 
 	for (i = 0; i < pow(len,2); i++) {
-		if (i % (len+1) == 0) {
-			h_d[i] = 1; res[i] = 1;
-		}
-		else {
-			h_d[i] = 0; res[i] = 0;
-		}
+		if (i % (len+1) == 0) h_d[i] = 1;
+		else h_d[i] = 0;
 	}
 
-	for (i = 0; i < reps; i++) {
-		// the glass implementation
-		cudaMemcpy(d_d, h_d, pow(len,2) * sizeof(double), cudaMemcpyHostToDevice);
-		cudaDeviceSynchronize();
-		clock_gettime(CLOCK_MONOTONIC, &start);
-		global_cholDecomp_InPlace<<<1,len>>>(len, d_d);
-		cudaDeviceSynchronize();
-		clock_gettime(CLOCK_MONOTONIC, &end);
-		time_taken = (double)end.tv_sec + (double)end.tv_nsec*1e-9
-                                - (double)start.tv_sec - (double)start.tv_nsec*1e-9;
-		if (i >= reps/2) {
-				total_time += time_taken;
-		}
+	cudaMalloc(&d_d, pow(len,2) * sizeof(double));
+	cudaMemcpy(d_d, h_d, pow(len,2) * sizeof(double), cudaMemcpyHostToDevice);
+	cudaDeviceSynchronize();
 
-		// seyoung's implementation
-		cudaMemcpy(d_d, h_d, pow(len,2) * sizeof(double), cudaMemcpyHostToDevice);
-		cudaDeviceSynchronize();
-		clock_gettime(CLOCK_MONOTONIC, &start);
-		global_cholDecomp_InPlace_vec<<<1,len>>>(len, d_d);
-		cudaDeviceSynchronize();
-		clock_gettime(CLOCK_MONOTONIC, &end);
-		time_taken = (double)end.tv_sec + (double)end.tv_nsec*1e-9
-                                - (double)start.tv_sec - (double)start.tv_nsec*1e-9;
-		if (i >= reps/2) {
-				total_time_vec += time_taken;
-		}
-	}
-	
+	// the glass implementation
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	global_cholDecomp_InPlace<<<1,len>>>(len, d_d, reps);
+	cudaDeviceSynchronize();
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	total_time = (double)end.tv_sec + (double)end.tv_nsec*1e-9
+				- (double)start.tv_sec - (double)start.tv_nsec*1e-9;
+
+	// seyoung's implementation
+	clock_gettime(CLOCK_MONOTONIC, &start);
+	global_cholDecomp_InPlace_vec<<<1,len>>>(len, d_d, reps);
+	cudaDeviceSynchronize();
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	total_time_vec = (double)end.tv_sec + (double)end.tv_nsec*1e-9
+							- (double)start.tv_sec - (double)start.tv_nsec*1e-9;
 	cudaFree(d_d);
-	free(h_d); free(res);
+	free(h_d);
 
 	// print average time
-	avg_time = total_time/reps*2;
-	avg_time_vec= total_time_vec/reps*2;
+	avg_time = total_time/reps;
+	avg_time_vec= total_time_vec/reps;
 	printf("avg time (glass):   %.10fs\n", avg_time);
 	printf("avg time (seyoung): %.10fs\n", avg_time_vec);
 	return 0;
