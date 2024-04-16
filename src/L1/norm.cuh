686e8af69d88b7357ef02dfeb47cc0ff4da224a4
Eric Feng <ef2648@columbia.edu>
2024-04-16 00:45:43 -0400

    formatting

--- src/L1/norm.cuh
+++ src/L1/norm.cuh
@@ -1,22 +1,24 @@
-#include <cstdint>
-#include <cooperative_groups.h>
 #include "reduce.cuh"
+#include <cooperative_groups.h>
+#include <cstdint>
 #include <math.h>
 
 namespace cgrps = cooperative_groups;
 
-template <typename T> __device__ T square(T N) {
-	return N * N;
+template <typename T> __device__ T square(T N)
+{
+    return N * N;
 }
 
-template <typename T> __device__ void vector_norm(std::uint32_t N, T *a, T *out, cgrps::thread_group g = cgrps::this_thread_block())
+template <typename T>
+__device__ void vector_norm(std::uint32_t N, T *a, T *out, cgrps::thread_group g = cgrps::this_thread_block())
 {
     for (int i = g.thread_rank(); i < N; i += g.size())
     {
-		out[i] = a[i] * a[i];
+        out[i] = a[i] * a[i];
     }
-	__syncthreads();
-	reduce(N, out, g);
-	__syncthreads();
+    __syncthreads();
+    reduce(N, out, g);
+    __syncthreads();
     out[0] = sqrt(out[0]);
 }
