9a53c5c49f3fb5efe3d6ac66e5ce8e489281ef47
Gbravop <gabobravop@gmail.com>
2025-04-29 18:57:23 -0400

    Merged changes

--- src/L1/axpy.cuh
+++ src/L1/axpy.cuh
@@ -23,6 +23,32 @@ void axpy(std::uint32_t n,
 /*
     Compute the scaled sum of two vectors
     alpha * x + y
+    store the result back in y, with a shift for y 
+*/
+template <typename T>
+__device__
+void axpy_shifted_y(std::uint32_t n,
+          std::uint32_t m, 
+          T alpha, 
+          T *x, 
+          T *y, 
+          cgrps::thread_group g = cgrps::this_thread_block())
+{
+    int k = n*m;
+    int s = 0;
+    int ind_y;
+    for(std::uint32_t ind = g.thread_rank(); ind < k; ind += g.size()){
+        if((s+1)%m == 0){
+            s += 1;
+        }
+        ind_y = ind + (n-m)*s;
+        y[ind_y] = alpha * x[ind] + y[ind_y];
+    }
+}
+
+/*
+    Compute the scaled sum of two vectors
+    alpha * x + y
     store the result in z
 */
 template <typename T>
